17b0972e625123ec201de9861786978867c619bb
Norman Torres <norman.torres.mx@gmail.com>
2023-10-06 22:47:02 -0600

    Add codigo

---
+++ app.test.js
@@ -0,0 +1,15 @@
+// Importa las librerías
+const request = require('supertest');
+const app = require('./index');  // Cambia esto a la ruta de tu archivo app
+
+// Test para verificar que la ruta '/' devuelve 'Hello World2'
+describe('GET /', () => {
+  it('should return Hello World2', async () => {
+    // Hace la petición
+    const res = await request(app).get('/');
+    
+    // Espera que el status sea 200 y el texto sea 'Hello World2'
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toEqual('Hello World2');
+  });
+});
--- index.js
+++ index.js
@@ -6,6 +6,4 @@ app.get('/', (req, res) => {
   res.send('Hello World2');
 })
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
-})
+module.exports = app;
