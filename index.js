64fb06aebb05a6c4be8c1ec70946809f24e96597
yuriGlobalGas <132495021+yuriGlobalGas@users.noreply.github.com>
2024-01-27 13:51:34 -0600

    Update index.js
    practica 1 k8

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const os=requiere('es');
+const hostname =os.hostname();
+console.log("El nombre de la maquina es",hostname);
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello new World 2');
+  res.send("El nombre de la maquina es",hostname);
 })
 
 module.exports = app;
